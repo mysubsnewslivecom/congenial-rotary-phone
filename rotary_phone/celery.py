d29a111fd03ae4c980caf864ab4eab2599c29f8c
mysubsnewslivecom <my.subs.news@live.com>
2022-10-30 21:48:47 +0100

    Added current temp

---
+++ rotary_phone/celery.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+
+from os import environ
+
+from celery import Celery
+environ.setdefault("DJANGO_SETTINGS_MODULE", "rotary_phone.settings")
+app = Celery(main="rotary_phone")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
